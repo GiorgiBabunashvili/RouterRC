b6cea72ece8206cab462b9adb87fad1b932cf8f1
shanME <giorgie103188@gmail.com>
2024-08-21 17:19:48 +0400

    usePArams

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>eventId: {params.eventId}</p>
+    </>
+  );
 };
 
 export default EventDetailPage;
